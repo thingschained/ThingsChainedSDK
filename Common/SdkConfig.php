1effbf2d9c1c14e4e1df8f0a8462c4a6448ac804
Jerry <cuiliang.hu@thingschained.com>
2018-03-18 09:47:42 +0800

    first commit

---
+++ Common/SdkConfig.php
@@ -0,0 +1,14 @@
+<?php
+namespace ThinksChained\Common;
+
+class SdkConfig
+{
+    const BASE_URI = 'http://127.0.0.1:8088'; //API网关
+
+    const KEY = 'xxxxxxxxxxxxx'; //调用API需要的key
+    
+    const SECRET = 'xxxxxxxxxxxxx'; //调用API需要的secret
+
+    const  THIRD_URI = 'http://127.0.0.1'; //第三方用于展示详情的URI
+}
+?>
---
+++ README.md
@@ -0,0 +1,13 @@
+基于星辰亿链区块链标准接口封装的一套PHP SDK
+
+## 如何使用？
+
+1、向[星辰亿链](http://www.thingschained.com)官方获取节点地址、key和secret
+
+2、将获得的节点地址、key和secret更新到Common/SdkConfig.php文件中
+
+
+
+
+
+
